fcc83c3d597f1ca8ce8e4b848d4bfdc27c97f791
Reeginaa <49926225+Reeginaa@users.noreply.github.com>
2019-06-12 18:12:04 -0300

    Update formCadastro.php

--- formCadastro.php
+++ formCadastro.php
@@ -17,17 +17,19 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
-    <form action="cadastrar.php" method="get" accept-charset="utf-8">
-      Nome Completo<br>
-      <input type="text" name="nome"><br>
-      Data de Nascimento<br>
-      <input type="date" name="nascimento"><br>
-      E-mail<br>
-      <input type="text" name="email"><br>
-      Senha<br>
-      <input type="password" name="senha"><br>
-      <input type="submit" name="enviar"value="Cadastrar">
-    </form>
+    <div id="formCadastro">
+      <form action="cadastrar.php" method="get" accept-charset="utf-8">
+        Nome Completo<br>
+        <input type="text" name="nome"><br>
+        Data de Nascimento<br>
+        <input type="date" name="nascimento"><br>
+        E-mail<br>
+        <input type="text" name="email"><br>
+        Senha<br>
+        <input type="password" name="senha"><br>
+        <input type="submit" name="enviar"value="Cadastrar">
+      </form>
+    </div> 
     <br><a href="indexLogin.php"><button>Voltar</button></a>
   </body>
 </html>
